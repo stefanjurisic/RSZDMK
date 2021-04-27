9faeae44fde334d5561c63783d9c310117b69590
“stefanjurisic” <stefanjurisic99@gmail.com>
2021-04-27 14:57:16 +0200

    Initial code added

--- vezba1/zadatak1/main.c
+++ vezba1/zadatak1/main.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 
 int main()
-
 {
-	printf("Hello World");
+	int a = 5;
+	int b = 4;
+	int c;
 	
+	if(a > b)
+		c = a - b;
+	else
+	
+	c = b - a;
+
+	printf("Result: %d\n", c);
+
 	return 0;
 }
